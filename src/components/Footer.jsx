f0012d744b7fd2d573ab94519c8c4c793d1ac45b
rudra moradiya <rudramoradiya123@gmail.com>
2026-07-16 16:41:05 +0530

    modification

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,9 +21,8 @@ const Footer = () => {
 
         {/* Ikon Sosmed - di tengah di mobile */}
         <div className="flex gap-3 order-2 md:order-none">
-          <a href="https://github.com/rissss21"><i className="ri-github-fill ri-2x"></i></a>
-          <a href="https://www.instagram.com/farisedrikprayoga/"><i className="ri-instagram-fill ri-2x"></i></a>
-          <a href="https://www.youtube.com/@FarisEdrikPrayoga"><i className="ri-youtube-fill ri-2x"></i></a>
+          <a href="https://github.com/rudramoradiya"><i className="ri-github-fill ri-2x"></i></a>
+          <a href="https://www.instagram.com/_rudra_moradiya_/"><i className="ri-instagram-fill ri-2x"></i></a>
         </div>
 
         {/* Dock - paling bawah di mobile */}
